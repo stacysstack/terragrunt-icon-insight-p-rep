cfda212e1d74625c1339947cdc6c35915983ec27
Stacy <alme0056@umn.edu>
2019-10-05 17:54:28 -0700

    added c2 destroy script

---
+++ aws/environments/dev/destroy-c2-single-prep.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+# Cache the plugins
+mkdir -p ~/.terraform.d/plugin-cache
+export TF_PLUGIN_CACHE_DIR=~/.terraform.d/plugin-cache
+export TF_INPUT=0
+
+DIRECTORIES=( \
+"us-east-1/citizen/ec2-ddos" \
+"us-east-1/citizen/sg" \
+"us-east-1/citizen/keys" \
+"us-east-1/p-rep/ec2-configuration-c1" \
+"us-east-1/p-rep/ec2" \
+"us-east-1/p-rep/sg" \
+"us-east-1/p-rep/keys" \
+"us-east-1/network/vpc" \
+"us-east-1/logging/lb-logging-bucket" \
+"us-east-1/logging/log-config-bucket" \
+"global/profiles/citizen" \
+"global/profiles/p-rep" \
+
+)
+
+for i in "${DIRECTORIES[@]}"
+do
+   terragrunt destroy --terragrunt-source-update --terragrunt-non-interactive --auto-approve --terragrunt-working-dir $i
+done
